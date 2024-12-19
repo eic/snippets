fa3322869e7b10f47b8e21f42abddfc94715a6ee
ruse-traveler <derek.murphy.anderson@protonmail.com>
2024-12-19 13:03:15 -0500

    Simplify script

--- Calorimetery/CaloDebugTools/UtilityScripts/RunEICRecon.rb
+++ Calorimetery/CaloDebugTools/UtilityScripts/RunEICRecon.rb
@@ -1,11 +1,11 @@
 #!/usr/bin/env ruby
-# -----------------------------------------------------------------------------
-# 'RunEICRecon.rb'
-# Derek Anderson
-# 05.11.2023
+# =============================================================================
+# @file   RunEICRecon.rb
+# @author Derek Anderson
+# @date   05.11.2023
 #
 # An easy script to interface with EICRecon
-# -----------------------------------------------------------------------------
+# =============================================================================
 
 # i/o parameters
 in_ddsim   = "../forTestingJetPodioRelations.edm4hep.root"
@@ -19,44 +19,19 @@ out_collect = [
   "GeneratedChargedJets",
   "ReconstructedJets",
   "ReconstructedChargedJets"
-]
+].compact.reject(&:empty?).join(',')
 
 # plugins to run in EICrecon
 plugins = [
   "dump_flags"
-]
+].compact.reject(&:empty?).join(',')
 
 # options
 options = [
-  "-Preco:ReconstructedJets:minJetPt=2",
-  "-Preco:ReconstructedJets:minCstPt=2"
-]
-
-# create collection argument
-arg_collect = ""
-num_collect = out_collect.size
-out_collect.each_with_index do |collect, iCollect|
-  arg_collect += collect
-  arg_collect += "," if iCollect + 1 != num_collect
-end
-
-# create plugin argument
-arg_plugin = ""
-num_plugin = plugins.size
-plugins.each_with_index do |plugin, iPlugin|
-  arg_plugin += plugin
-  arg_plugin += "," if iPlugin + 1 != num_plugin
-end
-
-# create options argument
-arg_option = ""
-num_option = options.size
-options.each_with_index do |option, iOption|
-  arg_option += option
-  arg_option += " " if iOption + 1 != num_option
-end
+  "-Peicrecon:LogLevel=debug"
+].compact.reject(&:empty?).join(' ')
 
 # run EICrecon
-exec("eicrecon -Pplugins=#{arg_plugin} -Ppodio:output_collections=#{arg_collect} -Ppodio:output_file=#{out_podio} -Phistsfile=#{out_plugin} #{in_ddsim}")
+exec("eicrecon -Pplugins=#{plugins} -Ppodio:output_collections=#{out_collect} #{options} -Ppodio:output_file=#{out_podio} -Phistsfile=#{out_plugin} #{in_ddsim}")
 
-# end -------------------------------------------------------------------------
+# end =========================================================================
