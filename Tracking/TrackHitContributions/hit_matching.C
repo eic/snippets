981b2f1543a640fc61fed6bebfe6b90c36884d69
Barak Schmookler <bschmookler1@gmail.com>
2025-05-29 16:40:44 -0500

    Updated track hit matching script

--- Tracking/TrackHitContributions/hit_matching.C
+++ Tracking/TrackHitContributions/hit_matching.C
@@ -4,7 +4,7 @@
 #include <edm4eic/TrackCollection.h>
 #include <edm4eic/MCRecoTrackerHitAssociationCollection.h>
 #include <podio/Frame.h>
-#include <podio/ROOTFrameReader.h>
+#include <podio/ROOTReader.h>
 
 //SimHit collections
 std::vector<std::string> sim_coll_names{
@@ -24,7 +24,16 @@ std::vector<std::string> rec_coll_names{
 std::vector<unsigned int> rec_coll_ids;
 
 //Simulation file to use
+//std::string input_file = "input/eicrecon_etof_test_local.root";
+//std::string input_file = "input/eicrecon_etof_test.root";
 std::string input_file = "input/eicrecon_out_2GeV.root";
+
+
+//------------------
+//Template to access 'sign' of radius in (x,y) plane
+template <typename T> int sgn(T val) {
+    return (T(0) < val) - (val < T(0));
+}
 
 //-----------------
 //Function to set Collection IDs
@@ -63,6 +72,12 @@ void hit_matching(){
 
 	//Print out information event-by-event
 	bool print_evt_info = false;
+
+	//Using new Barrel MPDG digitization
+	bool new_mpgd_digi = false;
+
+	//Plot x-y positions of last Forward TOF layer
+	bool ftof_back_xy = false;
 
 	//Set collection Ids
 	rec_coll_ids = get_coll_ids(rec_coll_names);
@@ -137,24 +152,24 @@ void hit_matching(){
 	h7->GetYaxis()->SetTitle("Number of tracks");h7->GetYaxis()->CenterTitle();
 
 	//Number of RecHits per collection
-	//1) All associated RecHits for events w/ 1 track
-	//2) All associated measurement RecHits for events w/ 1 track
-	//3) All associated outlier RecHits for events w/ 1 track
+	//1) All associated RecHits for events w/ a reconstructed track
+	//2) All associated measurement RecHits for events w/ a reconstructed track
+	//3) All associated outlier RecHits for events w/ a reconstructed track
 	int nBins = rec_coll_names.size();
 
-    	TH1F *h8a = new TH1F("h8a","Events with 1 reconstructed track", nBins, 0, nBins); //Associated digitized hits
+    	TH1 *h8a = new TH1D("h8a","Events with a reconstructed track", nBins, 0, nBins); //Associated digitized hits
 	h8a->SetLineWidth(3);h8a->SetLineColor(kBlue);
 	h8a->GetYaxis()->SetTitle("Counts");h8a->GetYaxis()->CenterTitle();
 
-	TH1F *h8b = new TH1F("h8b","Events with 1 reconstructed track", nBins, 0, nBins); //Associated track measurement hits
+	TH1 *h8b = new TH1D("h8b","Events with a reconstructed track", nBins, 0, nBins); //Associated track measurement hits
         h8b->SetFillColor(kGreen+2);h8b->SetFillStyle(3004);h8b->SetLineWidth(0);
         h8b->GetYaxis()->SetTitle("Counts");h8b->GetYaxis()->CenterTitle();
 
-	TH1F *h8c = new TH1F("h8c","Events with 1 reconstructed track", nBins, 0, nBins); //Associated outlier hits
+	TH1 *h8c = new TH1D("h8c","Events with a reconstructed track", nBins, 0, nBins); //Associated outlier hits
         h8c->SetFillColor(kRed);h8c->SetFillStyle(3005);h8c->SetLineWidth(0);
         h8c->GetYaxis()->SetTitle("Counts");h8c->GetYaxis()->CenterTitle();
 
-    	// Assign bin labels
+    	//Assign bin labels
     	for (size_t i = 0; i < rec_coll_names.size(); i++) {
         	h8a->GetXaxis()->SetBinLabel(i + 1, rec_coll_names[i].c_str());
 		h8b->GetXaxis()->SetBinLabel(i + 1, rec_coll_names[i].c_str());
@@ -164,22 +179,58 @@ void hit_matching(){
 	//Number of RecHits per collection
         //1) All associated RecHits
         //2) All associated Measurement2D Hits
-        TH1F *h9a = new TH1F("h9a","", nBins, 0, nBins); //Associated RecHits
+        TH1 *h9a = new TH1D("h9a","", nBins, 0, nBins); //Associated RecHits
         h9a->SetLineWidth(3);h9a->SetLineColor(kBlue);
         h9a->GetYaxis()->SetTitle("Counts");h9a->GetYaxis()->CenterTitle();
 
-        TH1F *h9b = new TH1F("h9b","", nBins, 0, nBins); //Associated Measurement2D
+        TH1 *h9b = new TH1D("h9b","", nBins, 0, nBins); //Associated Measurement2D
         h9b->SetFillColor(kMagenta+2);h9b->SetFillStyle(3004);h9b->SetLineWidth(0);
         h9b->GetYaxis()->SetTitle("Counts");h9b->GetYaxis()->CenterTitle();
 
-	// Assign bin labels
+	//Assign bin labels
 	for (size_t i = 0; i < rec_coll_names.size(); i++) {
                 h9a->GetXaxis()->SetBinLabel(i + 1, rec_coll_names[i].c_str());
 		h9b->GetXaxis()->SetBinLabel(i + 1, rec_coll_names[i].c_str());
 	}
-	
-	podio::ROOTFrameReader r;
-	r.openFile(input_file); //Single muon, 2GeV/c momentum
+
+	//R vs. Z: Events with a reconstructed track
+	TH2 *hh1a = new TH2D("hh1a","All associated digitized hits",1000,-1800,2000,1000,-800,800);
+	hh1a->GetXaxis()->SetTitle("z [mm]");hh1a->GetXaxis()->CenterTitle();
+	hh1a->GetYaxis()->SetTitle("signed r [mm]");hh1a->GetYaxis()->CenterTitle();
+
+	TH2 *hh1b = new TH2D("hh1b","Associated track measurement hits",1000,-1800,2000,1000,-800,800);
+	hh1b->GetXaxis()->SetTitle("z [mm]");hh1b->GetXaxis()->CenterTitle();
+	hh1b->GetYaxis()->SetTitle("signed r [mm]");hh1b->GetYaxis()->CenterTitle();
+
+	TH2 *hh1c = new TH2D("hh1c","Associated track outlier hits",1000,-1800,2000,1000,-800,800);
+	hh1c->GetXaxis()->SetTitle("z [mm]");hh1c->GetXaxis()->CenterTitle();
+	hh1c->GetYaxis()->SetTitle("signed r [mm]");hh1c->GetYaxis()->CenterTitle();
+
+	TH2 *hh1d = new TH2D("hh1d","Associated digitized hits missing from track",1000,-1800,2000,1000,-800,800);
+	hh1d->GetXaxis()->SetTitle("z [mm]");hh1d->GetXaxis()->CenterTitle();
+	hh1d->GetYaxis()->SetTitle("signed r [mm]");hh1d->GetYaxis()->CenterTitle();	
+
+	//Forward TOF (last layer) y vs. x: Events with a reconstructed track
+	TH2 *hh2a = new TH2D("hh2a","All associated digitized hits",1000,-600,600,1000,-600,600);
+        hh2a->GetXaxis()->SetTitle("x [mm]");hh2a->GetXaxis()->CenterTitle();
+        hh2a->GetYaxis()->SetTitle("y [mm]");hh2a->GetYaxis()->CenterTitle();
+
+        TH2 *hh2b = new TH2D("hh2b","Associated track measurement hits",1000,-600,600,1000,-600,600);
+        hh2b->GetXaxis()->SetTitle("x [mm]");hh2b->GetXaxis()->CenterTitle();
+        hh2b->GetYaxis()->SetTitle("y [mm]");hh2b->GetYaxis()->CenterTitle();
+
+        TH2 *hh2c = new TH2D("hh2c","Associated track outlier hits",1000,-600,600,1000,-600,600);
+        hh2c->GetXaxis()->SetTitle("x [mm]");hh2c->GetXaxis()->CenterTitle();
+        hh2c->GetYaxis()->SetTitle("y [mm]");hh2c->GetYaxis()->CenterTitle();
+
+        TH2 *hh2d = new TH2D("hh2d","Associated digitized hits missing from track",1000,-600,600,1000,-600,600);
+        hh2d->GetXaxis()->SetTitle("x [mm]");hh2d->GetXaxis()->CenterTitle();
+        hh2d->GetYaxis()->SetTitle("y [mm]");hh2d->GetYaxis()->CenterTitle();	
+
+
+	//Open file	
+	podio::ROOTReader r;
+	r.openFile(input_file);
 
 	auto nevents = r.getEntries(podio::Category::Event);
 	cout<<"---------------"<<endl;
@@ -236,11 +287,11 @@ void hit_matching(){
 				if(mc_status==1 && quality==0) matched_simhits++;
 				tot_simhits++;
 
-				//For Barrel MPGDs, a single SimHit now usually gets converted into 2 Raw/Rec Hits
+				//For new Barrel MPGDs digitization, a single SimHit usually is converted into 2 RecHits
 				//So, we add an additional count for those to keep the numbers consistent
-				if( coll_name=="MPGDBarrelHits" || coll_name=="OuterMPGDBarrelHits"){
+				if( new_mpgd_digi && (coll_name=="MPGDBarrelHits" || coll_name=="OuterMPGDBarrelHits") ){
 					if(mc_status==1 && quality==0) matched_simhits++;
-                                	tot_simhits++;					
+                                	tot_simhits++;
 				}
 			} //Loop over SimHits in a given collection
 		} //Loop over SimHit collections
@@ -271,8 +322,11 @@ void hit_matching(){
 				auto hit_index = hit.getObjectID().index;
 				auto hit_cellid = hit.getCellID();
 
-				//Hit positions
-				//printf("%f | %f | %f \n\n", hit.getPosition().x,hit.getPosition().y,hit.getPosition().z);
+				//RecHit positions
+				auto hit_x = hit.getPosition().x;
+				auto hit_y = hit.getPosition().y;
+				auto hit_z = hit.getPosition().z;
+				double hit_radius = sgn<double>(hit_x) * sqrt(hit_x*hit_x + hit_y*hit_y);
 				
 				if(print_evt_info)
 					printf("%u | %d | %lu \n",hit_collid, hit_index, hit_cellid);
@@ -290,12 +344,16 @@ void hit_matching(){
 						if(mc_status==1 && quality==0){ 
 							tot_rechits++;
 						
-							//If there is 1 track, fill histogram of collection RecHits
+							//If there is at least 1 track, fill histogram of collection RecHits
 							auto& track_coll = f.get<edm4eic::TrackCollection>(trk_coll);
                 					auto num_tracks = track_coll.size();
 
-							if(num_tracks == 1 && (index_map.find(coll_name) != index_map.end()) ){
+							if(num_tracks > 0 && (index_map.find(coll_name) != index_map.end()) ){
 								h8a->Fill( index_map[coll_name] );
+								hh1a->Fill(hit_z,hit_radius);
+
+								if(coll_name=="TOFEndcapRecHits" && hit_z>1860.)
+									hh2a->Fill(hit_x,hit_y);
 							}
 							
 							//Without track requirement
@@ -375,7 +433,13 @@ void hit_matching(){
 
 				auto cellid = rec_hit.getCellID();
 				auto collid = rec_hit.getObjectID().collectionID;
-			
+		
+				//RecHit positions
+                        	auto hit_x = rec_hit.getPosition().x;
+                        	auto hit_y = rec_hit.getPosition().y;
+                        	auto hit_z = rec_hit.getPosition().z;
+                        	double hit_radius = sgn<double>(hit_x) * sqrt(hit_x*hit_x + hit_y*hit_y);
+	
 				//Get collection name for RecHit	
 				std::string coll_name;
 				if (RecHitCollMap.find(collid) != RecHitCollMap.end())
@@ -399,8 +463,12 @@ void hit_matching(){
 							num_meas_corr++;
 				
 							//Fill associated measurement hit collection histogram
-							if(num_tracks == 1 && (index_map.find(coll_name) != index_map.end()) ){
+							if( (index_map.find(coll_name) != index_map.end()) ){
                                                                 h8b->Fill( index_map[coll_name] );
+								hh1b->Fill(hit_z,hit_radius);
+
+								if(coll_name=="TOFEndcapRecHits" && hit_z>1860.)
+                                                                        hh2b->Fill(hit_x,hit_y);
                                                         }
 
 						} //If hit associated to primary particle
@@ -426,6 +494,12 @@ void hit_matching(){
 				auto cellid = out_hit.getCellID();
 				auto collid = out_hit.getObjectID().collectionID;
 
+				//RecHit positions
+                                auto hit_x = out_hit.getPosition().x;
+                                auto hit_y = out_hit.getPosition().y;
+                                auto hit_z = out_hit.getPosition().z;
+                                double hit_radius = sgn<double>(hit_x) * sqrt(hit_x*hit_x + hit_y*hit_y);
+				
                                 //Get collection name for RecHit
                                 std::string coll_name;
                                 if (RecHitCollMap.find(collid) != RecHitCollMap.end())
@@ -448,9 +522,13 @@ void hit_matching(){
 							should_be_meas = "Y";
 							num_out_wrong++;
 
-							//Fill associated measurement hit collection histogram
-                                                        if(num_tracks == 1 && (index_map.find(coll_name) != index_map.end()) ){
+							//Fill associated outlier hit collection histogram
+                                                        if( (index_map.find(coll_name) != index_map.end()) ){
                                                                 h8c->Fill( index_map[coll_name] );
+								hh1c->Fill(hit_z,hit_radius);
+
+								if(coll_name=="TOFEndcapRecHits" && hit_z>1860.)
+                                                                        hh2c->Fill(hit_x,hit_y);
                                                         }
 						}
 					}
@@ -486,7 +564,6 @@ void hit_matching(){
 			cout<<endl<<"---------------"<<endl;
 
 		//Fill histograms
-
 		if(num_tracks>0){ //Require at least 1 track
 			h1->Fill(matched_simhits);
 			if(matched_simhits>0) 
@@ -505,11 +582,29 @@ void hit_matching(){
 
 	} //End loop over events
 
+	//Scale and Subtract histograms
+	TH1* h8bs = (TH1D*)h8b->Clone("h8bs");
+	h8bs->GetYaxis()->SetTitle("Fraction of associated digitized hits");
+	h8bs->Divide(h8a);
+
+	TH1* h8cs = (TH1D*)h8c->Clone("h8cs");
+	h8cs->GetYaxis()->SetTitle("Fraction of associated digitized hits");
+        h8cs->Divide(h8a);
+
+	hh1d->Add(hh1a,hh1b,1,-1); //Subtract track measurement hits from associated hits
+	hh1d->Add(hh1c,-1); //Also subtract outlier hits
+
+	hh2d->Add(hh2a,hh2b,1,-1); //Subtract track measurement hits from associated hits
+        hh2d->Add(hh2c,-1); //Also subtract outlier hits
 
 	//Histogram stacking
 	THStack *stack1 = new THStack("stack1", "Stacked Histograms");
     	stack1->Add(h8b);
     	stack1->Add(h8c);
+
+	THStack *stack2 = new THStack("stack2", "Events with a reconstructed track; ; Fraction of associated digitized hits");
+        stack2->Add(h8bs);
+        stack2->Add(h8cs);
 
 	//Make plots
 	TCanvas *c1 = new TCanvas("c1");
@@ -552,6 +647,10 @@ void hit_matching(){
         legend->SetTextSize(0.035);
 	legend->Draw();
 
+	TCanvas *c8s = new TCanvas("c8s");
+	c8s->SetGrid();
+	stack2->Draw("HIST");
+
 	TCanvas *c9 = new TCanvas("c9");
 	h9a->Draw();
 	h9b->Draw("same");
@@ -564,6 +663,30 @@ void hit_matching(){
         legend2->SetTextSize(0.035);
         legend2->Draw();
 
+	TCanvas *c10a = new TCanvas("c10a");
+	hh1a->Draw();
+
+	TCanvas *c10b = new TCanvas("c10b");
+        hh1b->Draw();
+
+	TCanvas *c10c = new TCanvas("c10c");
+        hh1c->Draw();
+
+	TCanvas *c10d = new TCanvas("c10d");
+        hh1d->Draw();
+
+	TCanvas *c11a = new TCanvas("c11a");
+        hh2a->Draw();
+
+	TCanvas *c11b = new TCanvas("c11b");
+        hh2b->Draw();
+
+        TCanvas *c11c = new TCanvas("c11c");
+        hh2c->Draw();
+
+        TCanvas *c11d = new TCanvas("c11d");
+        hh2d->Draw();
+
 	//Print plots to file
 	cout<<endl;
 	c1->Print("plots/hit_matching.pdf[");
@@ -575,6 +698,15 @@ void hit_matching(){
 	c6->Print("plots/hit_matching.pdf");
 	c7->Print("plots/hit_matching.pdf");
 	c8->Print("plots/hit_matching.pdf");
+	c8s->Print("plots/hit_matching.pdf");
 	c9->Print("plots/hit_matching.pdf");
-	c9->Print("plots/hit_matching.pdf]");
+	c10a->Print("plots/hit_matching.pdf");
+	c10b->Print("plots/hit_matching.pdf");
+	c10c->Print("plots/hit_matching.pdf");
+	c10d->Print("plots/hit_matching.pdf");
+	c11a->Print("plots/hit_matching.pdf");
+	c11b->Print("plots/hit_matching.pdf");
+	c11c->Print("plots/hit_matching.pdf");
+	c11d->Print("plots/hit_matching.pdf");
+	c11d->Print("plots/hit_matching.pdf]");
 }
