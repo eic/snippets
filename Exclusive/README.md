fc07a3dd272eb4ffdc2fce4b3d5c091fbf4eb0e5
sjdkay <sjdkay@gmail.com>
2025-07-01 12:04:47 +0100

    Added a header file for a generic version of teXBABE calculations. This is adapted from a method specific for DEMP analysis.

--- Exclusive/README.md
+++ Exclusive/README.md
@@ -18,3 +18,35 @@ The header file here contains functions for calculating the following quantities
 
 
 The functions take as input the 4 vectors for particles identified within an ePIC event. These 4-vectors can take the form of any type which includes the relevant operators: `.E()`, `.P()`, `.Pt()` and `.M2()`. ROOT's legacy `TLorentzVector` class, and its more recent replacement, `ROOT::Math::LorentzVector`, both contain all of the operators required.
+
+## `tEXBABE.h`
+
+A header file to calculate teXBABE. This is a generic formulation of a calculation used in the analysis of DEMP events. Vectors and inputs have been named generically to match the tRECO convention for ease of use and readability.
+
+```
+#include "snippets/Exclusive/teXBABE.h"
+```
+
+The header file contains three methods for calculating teXBABE -
+
+- An explicit method which takes 6 inputs
+  - Detected scattered electron vector
+  - Detected X vector
+  - Detected baryon (BA) vector
+  - Mass of the expected baryon (BA)
+  - Electron beam vector
+  - Hadron beam vector
+- A simplified method with 4 inputs
+  - PMiss vector
+  - Detected BA vector
+  - Mass of the expected baryon (BA)
+  - Hadron beam vector
+- Shortest method with 2 inputs
+  - Corrected BA vector
+  - Hadron beam vector
+
+Some comments and additional explanations are included in the file.
+
+## Notes
+
+A test of equivalence between ePIC_ExcKinUtils.h and teXBABE.h should be conducted.
---
+++ Exclusive/tEXBABE.h
@@ -0,0 +1,33 @@
+// Stephen JD Kay - University of York
+// Stephen.Kay@york.ac.uk
+// 01/07/25
+// Header file for a generic method of calculating teXBABE as defined by the tRECO convention
+// This offers an alternative to ePIC_ExcKinUtils where ONLY teXBABE is defined and calculated - https://github.com/eic/snippets/blob/main/Exclusive/ePIC_ExcKinUtils.h
+// Method expects a three body final state where the expected baryon (BA) state is known and the scattered electron (eSc) and other hadronic state(s) X, are measured and well known
+
+PxPyPzEVector Vec_BACorr;
+
+// Explicit method to calculate teXBABE. Inputs are the detected scattered electron, detected X, detected baryon (BA), mass of the expected baryon, Electron beam and hadron beam
+// All inputs OTHER than the BA mass are expected as PxPyPzEVectors
+Double_t Calc_teXBABE(PxPyPzEVector Vec_eSc, PxPyPzEVector Vec_X, PxPyPzEVector Vec_BA, Double_t Mass_BA, PxPyPzEVector Vec_EBeam, PxPyPzEVector Vec_HBeam){
+  PxPyPzEVector Vec_PMiss = (Vec_EBeam + Vec_HBeam) - (Vec_eSc + Vec_X);
+  // "Correct" the BA vector - utilise knowledge of the final state and the POSITION resolution of the original BA track
+  Vec_BACorr.SetPxPyPzE(Vec_PMiss.P()*sin(Vec_BA.Theta())*cos(Vec_BA.Phi()), Vec_PMiss.P()*sin(Vec_BA.Theta())*sin(Vec_BA.Phi()), Vec_PMiss.P()*cos(Vec_BA.Theta()), sqrt(pow(Vec_PMiss.P(),2)+(pow(Mass_BA,2))));
+  Vec_teXBABE = ;
+  Double_t t_eXBABE = -1*((Vec_HBeam - Vec_BACorr).mag2());
+  return(t_eXBABE);
+}
+
+// Shortened method to calculate teXBABE where inputs are PMiss, BA vector, BA mass and HBeam only
+Double_t Calc_tEXBABE(PxPyPzEVector Vec_PMiss, PxPyPzEVector Vec_BA, Double_t Mass_BA, PxPyPzEVector Vec_HBeam){
+  // "Correct" the BA vector - utilise knowledge of the final state and the POSITION resolution of the original BA track
+  Vec_BACorr.SetPxPyPzE(Vec_PMiss.P()*sin(Vec_BA.Theta())*cos(Vec_BA.Phi()), Vec_PMiss.P()*sin(Vec_BA.Theta())*sin(Vec_BA.Phi()), Vec_PMiss.P()*cos(Vec_BA.Theta()), sqrt(pow(Vec_PMiss.P(),2)+(pow(Mass_BA,2))));
+  Double_t t_eXBABE = -1*((Vec_HBeam - Vec_BACorr).mag2());
+  return(t_eXBABE);
+}
+
+// Shortest method, inputs are corrected BA vector and HBeam only
+Double_t Calc_tEXBABE(PxPyPzEVector Vec_BACorr, PxPyPzEVector Vec_HBeam){
+  Double_t t_eXBABE = -1*((Vec_HBeam - Vec_BACorr).mag2());
+  return(t_eXBABE);
+}
