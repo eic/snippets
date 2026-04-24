18aa1d59f8ad2eb53494ba012000106b1ba25fcd
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-04-23 20:59:22 -0400

    Infrastructure/Acts_versions: add dd4hep variation

--- Infrastructure/Acts_versions/plot.py
+++ Infrastructure/Acts_versions/plot.py
@@ -2,10 +2,10 @@ from datetime import datetime, timedelta
 
 import numpy as np
 
-import matplotlib.dates as mdates
-import matplotlib.units as munits
-
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+COMPONENT = "DD4hep"
 
 @np.vectorize
 def to_datetime(date):
@@ -15,8 +15,12 @@ with open("tags") as fp:
     tags = np.array([l.strip().replace("refs/tags/v", "").split("|") for l in fp.readlines()])
 tags_dates = to_datetime(tags[:,1])
 order = np.argsort(tags_dates)
-tags = tags[order][101:]
-tags_dates = tags_dates[order][101:]
+skip = {
+    "Acts": 101,
+    "DD4hep": 61,
+}[COMPONENT]
+tags = tags[order][skip:]
+tags_dates = tags_dates[order][skip:]
 
 with open("eic_container_tags") as fp:
     eic_container_tags = np.array([l.strip().split("|") for l in fp.readlines()])
@@ -32,6 +36,7 @@ order = np.argsort(result_dates)
 result = result[order]
 result_dates = result_dates[order]
 
+assert len(eic_container_tags) == len(result)
 
 result_ixs, tags_ixs = np.nonzero(result[:,0][:,np.newaxis] == tags[:,0][np.newaxis,:])
 print(result_ixs)
@@ -47,17 +52,22 @@ def annotate(ax, label, x, y, xytext):
 fig, ax = plt.subplots(figsize=(6.4, 3), layout='constrained')
 
 x = np.arange(len(tags))
-ax.plot(tags_dates, x, label="Acts releases")
+ax.plot(tags_dates, x, label=f"{COMPONENT} releases")
 ax.plot(eic_container_tags_dates[result_ixs], tags_ixs, label="ePIC stack releases")
 
-ax.xaxis.set_major_locator(mdates.YearLocator())
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+ax.yaxis.set_major_locator(mpl.ticker.MaxNLocator(integer=True))
+ax.xaxis.set_major_locator(mpl.dates.YearLocator())
+ax.xaxis.set_major_formatter(mpl.dates.DateFormatter('%Y'))
 container_bump_ix = np.nonzero(result[1:,0] != result[:-1,0])[0] + 1
 assert np.allclose(result_ixs, range(len(result_ixs)))
 for cix, ix in zip(container_bump_ix, tags_ixs[container_bump_ix]):
-    annotate(plt.gca(), tags[ix,0], tags_dates[ix], ix, (-200 + ix, ix * 1.2 - 90))
-    annotate(plt.gca(), eic_container_tags[cix,0], eic_container_tags_dates[cix], ix, (-100 + ix, -80 + ix * 0.5))
-plt.ylabel("Acts version index", loc="top")
+    if COMPONENT == "Acts":
+        annotate(plt.gca(), tags[ix,0], tags_dates[ix], ix, (-200 + ix, ix * 1.2 - 90))
+        annotate(plt.gca(), eic_container_tags[cix,0], eic_container_tags_dates[cix], ix, (-100 + ix, -80 + ix * 0.5))
+    elif COMPONENT == "DD4hep":
+        annotate(plt.gca(), tags[ix,0], tags_dates[ix], ix, (-100 + 2 * ix, ix * 2.4 - 10))
+        annotate(plt.gca(), eic_container_tags[cix,0], eic_container_tags_dates[cix], ix, (-10 + 2 * ix, -80 + ix))
+plt.ylabel(f"{COMPONENT} version index", loc="top")
 plt.legend()
 ax.spines[['right', 'top']].set_visible(False)
 plt.savefig("fig.png", dpi=300)
