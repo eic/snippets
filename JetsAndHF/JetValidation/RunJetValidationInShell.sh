ad5f8ffd0171e5fb1c0be35da42bff955f1365d1
ruse-traveler <derek.murphy.anderson@protonmail.com>
2026-09-11 17:02:17 -0400

    Split jet macro into hist-making and plot-making parts

--- JetsAndHF/JetValidation/RunJetValidationInShell.sh
+++ JetsAndHF/JetValidation/RunJetValidationInShell.sh
@@ -9,6 +9,7 @@
 #!     RunJetValidationInShell.sh -p "./plots" \
 #!                                -s "my_plots" \
 #!                                -l "my_filelist.list" \
+#!                                -h "my_hist_file.root" \
 #!                                -f 100 \
 #!                                -e 1000
 #!
@@ -19,17 +20,20 @@
 out_path="."
 out_suffix="files26071.py8ncdis10x100q100t1000"
 file_list="filelists/files26071.py8ncdis10x100q100t1000.list"
+hist_file="hists.$out_suffix.root"
 num_files=-1
 num_events=-1
 
-while getopts "p:s:l:f:e:" opt; do
+while getopts "p:s:l:h:f:e:" opt; do
   case $opt in
     p) out_path=$OPTARG ;;
     s) out_suffix=$OPTARG ;;
     l) file_list=$OPTARG ;;
+    h) hist_file=$OPTARG ;;
     f) num_files="$OPTARG" ;;
     e) num_events="$OPTARG" ;;
   esac
 done
 
-root -b -q "JetValidation.C(\"$out_path\", \"$out_suffix\", \"$file_list\", $num_files, $num_events)"
+root -b -q "MakeJetValidationHists.C(\"$out_path\", \"$out_suffix\", \"$file_list\", $num_files, $num_events)"
+root -b -q "MakeJetValidationPlots.C(\"$out_path\", \"$out_suffix\", \"$hist_file\")"
